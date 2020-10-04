3ccf2698f216969f2a7a9b65bb7decbe4dadc887
Edwin Dixon <ejd83@uclive.ac.nz>
2020-10-04 23:44:14 +1300

    trying to make subject/predicament

--- translate2.py
+++ translate2.py
@@ -110,44 +110,6 @@ diction = get_dict()
 dictionp = {"where is": "e ngaa","who is": "antai", "how's": "e uara", "how is": "e uara", "why is": "Bukin tera bwa e", "what's up with" : "??????????"}
 
 
-
-
-#diction1 = {"go": "naako", "i'm": "ngai", "know": "ataia", "yes": "eng", "okay" : "e raoiroi", "ok": "e raoiroi", "o.k":"e raoiroi", "fine": "e raoiroi", "please": "taiaoka", "get": "ana", "new": "e boou", "come": "nako mai", "one": "teuana", "like": "te rain", "going": "e a nako", "wait": "taninga", "sorry": "kabwara au bure", "want": "i tangira", "city": "taun", "in": "i", "what": "tera", "leave": "naako"}
-
-
-
-
-#def translate(word, bi_word, tri_word):
-    #try:
-        #return 3, diction3[tri_word]
-    #except KeyError:
-        #pass
-    #try:
-        ##print(bi_word)
-        ##print(diction2[bi_word])
-        #if not tri_word.endswith(('it', 'he', 'she')):
-            #try:
-                #return 2, dictionp[bi_word] + tri_word.split()[2]
-            #except:
-                #return 2, dictionp[bi_word]
-        #return 2, dictionp[bi_word]
-    #except KeyError:
-        #pass    
-    #try:
-        ##print(bi_word)
-        ##print(diction2[bi_word])
-        #return 2, diction2[bi_word]
-    #except KeyError:
-        #pass
-    #try:
-        #return 1, diction1[word]
-    #except KeyError:
-        #pass
-    #try:
-        #return 1, diction[word]
-    #except KeyError:
-        #return 1, word
-
 def translate_file_attempt(title3, filename, film):
     try:
         translate_file(filename, film)
@@ -155,7 +117,87 @@ def translate_file_attempt(title3, filename, film):
     except:
         title3.set("Something went wrong!?! Maybe wrong filename")
         
-
+#def translate_file(filename, film):
+    #words = collections.deque()
+    #f_r = collections.deque()
+    #f = open(filename, 'r')
+    #lines = f.read().lower().split('\n\n')
+    #for j in range(len(lines)):
+        #line1 = lines[j].split()
+        ##print(line1)
+        #line = line1[4:]
+        ##print(line)
+        #line = " ".join(line)
+        #skips = 1
+        #i = 0
+        #if '.' in line:
+            #line = line.split('.')
+        #elif '-' in line:
+            #line = line.split('-')
+        #else:
+            #line = [line]
+        #for sentence in line:
+            #print(sentence)
+            #if contains_subject(sentence):
+                
+            ##for word in sentence:
+                ###print(line)
+                ###print(word, i)
+                ##f_r_tup, word1 = pre_adjust(word)
+                ##words.append(word1)
+                ##f_r.append(f_r_tup)
+        ##while 0 < len(words):
+            ##if skips > 1:
+                ##line[i] = ''
+                ##skips -= 1
+                ##words.popleft()
+                ##f_r.popleft()
+            ##else:
+                ##num = len(words)
+                ##skips, word = translate(words, num)
+                ##tup = f_r.popleft()
+                ##word = post_adjust(tup, word)
+                ###print(word)
+                ##line[i] = word
+                ##words.popleft()
+                ###f_r.popleft()
+            ##i += 1
+            
+        ###print(line)
+        ##try:
+            ##start = line1[0] + '\n' + line1[1] + ' '+ line1[2] + ' ' + line1[3] + '\n' 
+            ##line = ' '.join(line)
+            ##line = start + line
+            ###print(line)
+            ##lines[j] = line
+        ##except IndexError:
+            ##pass
+    ##string = '\n\n'.join(lines)
+    
+    ###print(string)
+    ##f.close()   
+    
+    
+    ##file = "{}-Kiribati.srt".format(film)
+    ##w = open(file, 'w')
+    ##w.write(string)
+    ##w.close()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 def translate_file(filename, film):
     words = collections.deque()
     f_r = collections.deque()
@@ -213,37 +255,37 @@ def translate_file(filename, film):
 
 
 if __name__ == '__main__':
-    translate_file("Jaws-English.srt", "Jaws")
-    #from tkinter import *
-    #from tkinter.ttk import *  
-    #window = Tk()
-    #frame = Frame(window,width=600,height=400)
-    #frame.pack(expand=YES)
-    #filename = StringVar()
-    #title = StringVar()
-    #title.set("Enter english subtitle filename:")
-    #filename.set("")
-    #label = Label(frame, textvariable=title, width=100)
-    #label.pack()
-    #entry = Entry(frame, textvariable=filename, width=100)
-    #entry.pack()
+    #translate_file("red.txt", "red")
+    from tkinter import *
+    from tkinter.ttk import *  
+    window = Tk()
+    frame = Frame(window,width=600,height=400)
+    frame.pack(expand=YES)
+    filename = StringVar()
+    title = StringVar()
+    title.set("Enter english subtitle filename:")
+    filename.set("")
+    label = Label(frame, textvariable=title, width=100)
+    label.pack()
+    entry = Entry(frame, textvariable=filename, width=100)
+    entry.pack()
     
-    #film_name = StringVar()
-    #title2 = StringVar()
-    #title2.set("Enter film name")
-    #film_name.set("")
-    #label2 = Label(frame, textvariable=title2, width=100)
-    #label2.pack()
-    #entry2 = Entry(frame, textvariable=film_name, width=100)
-    #entry2.pack()
+    film_name = StringVar()
+    title2 = StringVar()
+    title2.set("Enter film name")
+    film_name.set("")
+    label2 = Label(frame, textvariable=title2, width=100)
+    label2.pack()
+    entry2 = Entry(frame, textvariable=film_name, width=100)
+    entry2.pack()
     
-    #title3 = StringVar()
-    #title3.set("")
-    #label3 = Label(frame, textvariable=title3, width=100)
-    #label3.pack()
+    title3 = StringVar()
+    title3.set("")
+    label3 = Label(frame, textvariable=title3, width=100)
+    label3.pack()
     
     
-    #clear = Button(frame, text="Enter", width=100, command=lambda: translate_file_attempt(title3, filename.get(), film_name.get()))
-    #clear.pack()
+    clear = Button(frame, text="Enter", width=100, command=lambda: translate_file_attempt(title3, filename.get(), film_name.get()))
+    clear.pack()
     
-    #window.mainloop()
+    window.mainloop()
