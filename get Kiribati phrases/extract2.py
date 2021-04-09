2793cfdaedadc91a9023fbd0b65da28e453fed5e
edwindixon99 <49737629+edwindixon99@users.noreply.github.com>
2021-04-09 18:45:42 +1200

    finshed 3 : examples

--- get Kiribati phrases/extract2.py
+++ get Kiribati phrases/extract2.py
@@ -25,22 +25,7 @@ for i in alpha:
         count = 0
         for j in range(len(lines)):
             line = lines[j].rstrip()
-            #if line.startswith('   <TD><IMG SRC="empty10.gif" WIDTH=60 HEIGHT=10>ex</TD>'):
-                #nextline = lines[j+1].rstrip()
-                ##print(lines[j+1].rstrip())
-                ##final.append(str(count) + ' : ' + eng_word)
-                #translation = h.handle(nextline).strip('|\n')
-                ##print(translation)
-                ##count += 1
-                #translist = translation.split(":")
-                #if len(translist) == 2:
-                    #count += 1
-                    ##diction[eng_word] = kir_word 
-                    #[kir_word, eng_word] = translist
-                    #if eng_word != "":
-                        #final.append(eng_word + " : " + kir_word)
-                        
-            if line.startswith('   <TD><IMG SRC="empty10.gif" WIDTH=60 HEIGHT=10>idiom</TD>'):
+            if line.startswith('   <TD><IMG SRC="empty10.gif" WIDTH=60 HEIGHT=10>ex</TD>'):
                 nextline = lines[j+1].rstrip()
                 #print(lines[j+1].rstrip())
                 #final.append(str(count) + ' : ' + eng_word)
@@ -48,19 +33,35 @@ for i in alpha:
                 #print(translation)
                 #count += 1
                 translist = translation.split(":")
-                if len(translist) == 1:
+                if len(translist) == 3:
                     count += 1
+                    final.append(translation)
                     #diction[eng_word] = kir_word 
                     #[kir_word, eng_word] = translist
                     #if eng_word != "":
-                        #final.append(eng_word + " : " + kir_word)            
-                    final.append(translation)
-        #print(translist)
-            #if line.startswith("<FONT COLOR=darkred><B>"):
-                #count += 1
-                #kir_word = h.handle(line).strip('.\n')
-                #kir_word = re.sub(r'\d+', '', kir_word)
-                #kdef = h.handle(lines[j+1]).strip('.\n')
+                        #final.append(eng_word + " : " + kir_word)
+                        
+            #if line.startswith('   <TD><IMG SRC="empty10.gif" WIDTH=60 HEIGHT=10>idiom</TD>'):
+                #nextline = lines[j+1].rstrip()
+                ##print(lines[j+1].rstrip())
+                ##final.append(str(count) + ' : ' + eng_word)
+                #translation = h.handle(nextline).strip('|\n')
+                ##print(translation)
+                ##count += 1
+                #translist = translation.split(":")
+                #if len(translist) >= 3:
+                    #count += 1
+                    ##diction[eng_word] = kir_word 
+                    ##[kir_word, eng_word] = translist
+                    ##if eng_word != "":
+                        ##final.append(eng_word + " : " + kir_word)            
+                    #final.append(translation)
+        ##print(translist)
+            ##if line.startswith("<FONT COLOR=darkred><B>"):
+                ##count += 1
+                ##kir_word = h.handle(line).strip('.\n')
+                ##kir_word = re.sub(r'\d+', '', kir_word)
+                ##kdef = h.handle(lines[j+1]).strip('.\n')
         print(count)
                 
         
@@ -97,7 +98,7 @@ for i in alpha:
 final = sorted(final)
     
 final = '\n\n'.join(final)
-w = open("dictionarynumbidiom1.txt", 'w')
+w = open("dictionarynumbidiom13.txt", 'w')
 j = json.dumps(diction)
 w.write(final)
 w.close()
