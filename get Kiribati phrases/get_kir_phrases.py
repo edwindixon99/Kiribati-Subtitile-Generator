e3783a23adb2d6c528f2bb56622c5df729ee1cef
edwindixon99 <49737629+edwindixon99@users.noreply.github.com>
2021-04-10 15:03:35 +1200

    1000 new phrases found

--- get Kiribati phrases/get_kir_phrases.py
+++ get Kiribati phrases/get_kir_phrases.py
@@ -1,11 +1,15 @@
+import re
+
 filename = "dictionarynumbidiom1.txt"
 
 lines = open(filename, "r").readlines()
 
-print(lines)
+#print(lines)
 
 final = []
+c = 0
 
+bl = ["", " ", "etc.", "etc", "..."]
 for i in range(len(lines)):
     line = lines[i].rstrip().strip()
     #print(line)
@@ -13,18 +17,75 @@ for i in range(len(lines)):
     line = line.split(":")
     
     if len(line) == 2:
-        kirs = line[0].split(",")
-        engs = line[1].split(",")
+        c += 1
+        #kirs = line[0].split(",")
+        #engs = line[1].split(",")
+        rese = re.split('[?!,]', line[1])
+        resk = re.split('[?!,]', line[0]) 
+        print(rese)
+        if (len(rese) > 1) and len(rese) == len(resk):
+            for p in range(len(rese)):
+                #print(rese[p])
+                #print()
+                ef, kf = rese[p].strip(), resk[p].strip()
+                if ef not in bl and kf not in bl:
+                    final.append("{} : {}".format(ef, kf))                    
+                #final.append("{} : {}".format(rese[p].strip(), resk[p].strip()))
+        elif len(rese) > 1 or len(resk) > 1:
+            for k1 in resk:
+                for e1 in rese:
+                    ef, kf = e1.strip(), k1.strip()
+                    if ef not in bl and kf not in bl:
+                        final.append("{} : {}".format(ef, kf))                        
+                    #final.append("{} : {}".format(e1.strip(), k1.strip()))
         
-        for k in kirs:
-            for e in engs:
-                k = k.strip()
-                e = e.strip()
-                final.append("{} : {}".format(e, k))
+        else:
+                          
+            ef, kf = rese[0].strip(), resk[0].strip()
+            
+            if ef not in bl and kf not in bl:
+                final.append("{} : {}".format(ef, kf))              
+            
+        #if ef not in bl and kf not in bl:
+            #final.append("{} : {}".format(ef, kf))        
+        
+        #for k in kirs:
+            #for e in engs:
+                #k = k.strip()
+                #e = e.strip()
+                
+                #rese = re.split('[?!,]', e)
+                #resk = re.split('[?!,]', k)
+                
+                #if (len(rese) > 1) and len(rese) == len(resk):
+                    #for p in range(len(rese)):
+                        ##print(rese[p])
+                        ##print()
+                        #ef, kf = rese[p].strip(), resk[p].strip()
+                        ##final.append("{} : {}".format(rese[p].strip(), resk[p].strip()))
+                #elif len(rese) > 1 or len(resk) > 1:
+                    #for k1 in rese:
+                        #for e1 in rese:
+                            #ef, kf = e1.strip(), k1.strip()
+                            ##final.append("{} : {}".format(e1.strip(), k1.strip()))
+                
+                #else:
+                    #ef, kf = e, k
+                    
+                #if ef not in bl and kf not in bl:
+                    #final.append("{} : {}".format(ef, kf))
+                
          
     lines[i] = line
-    
+#print(c)    
+#print(final)
+#print(lines)
+#print("ok")
 
-print(final)
-print(lines)
-print("ok")
+
+
+
+final = '\n\n'.join(final)
+w = open("dictionaryeng-kir.txt", 'w')
+w.write(final)
+w.close()
